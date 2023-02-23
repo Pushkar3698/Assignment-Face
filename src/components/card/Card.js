658b409ca730af7b54277dec8dd1753fe5b6b302
Pushkar3698 <pushkar3698@gmail.com>
2023-02-23 16:46:22 +0530

    framer-motion-added

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./style.css";
+import { motion } from "framer-motion";
+
 const Card = ({ name, image }) => {
   return (
-    <div className="card-container">
+    <motion.div
+      className="card-container"
+      initial={{ scale: 0.7, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      transition={{ ease: "easeInOut", duration: 0.4 }}
+    >
       <div className="user-img">
         <img src={image} alt="" />
       </div>
       <div className="user-name">{name}</div>
-    </div>
+    </motion.div>
   );
 };
 
